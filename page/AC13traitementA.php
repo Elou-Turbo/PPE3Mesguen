a0eb56ef1dab12637aae6ec2d135dbf356fe4259
Theo-Turbo <55579668+Theo-Turbo@users.noreply.github.com>
2019-09-27 08:46:54 +0200

    Update AC13traitementA.php

--- page/AC13traitementA.php
+++ page/AC13traitementA.php
@@ -23,7 +23,7 @@
 	$result = executeSQL($sql);
 	
 	if ($result)
-		echo "<meta http-equiv='refresh' content='0;url=AC11.php?message=<font color=green> Ajout realisee ! </font> <input id=\"tournee\" name=\"tournee\" type=\"hidden\" value=\"<?php echo \"$TRNNUM\" ?>\" />'>";
+		echo "<meta http-equiv='refresh' content='0;url=AC11.php?message=<font color=green> Ajout realisee ! </font> <input id=\"tournee\" name=\"tournee\" type=\"hidden\" value=\"<?php echo \"$TRNNUM\" ?>\'>";
 		else
 			echo "<meta http-equiv='refresh' content='0;url=AC11.php?message=<font color=red> Probleme pour ajouter ... </font>'>";
 ?>
