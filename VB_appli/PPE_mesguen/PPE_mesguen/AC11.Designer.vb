2b472e3318b389772bcbe98a4a980479a0e132e2
Theo-Turbo <55579668+Theo-Turbo@users.noreply.github.com>
2019-10-04 10:41:51 +0200

    AC11
    affichage table tournée

---
+++ VB_appli/PPE_mesguen/PPE_mesguen/AC11.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AC11
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableTournee = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.cmbBases = New System.Windows.Forms.ListBox()
+        Me.cmbTable = New System.Windows.Forms.ListBox()
+        CType(Me.TableTournee, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableTournee
+        '
+        Me.TableTournee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.TableTournee.Location = New System.Drawing.Point(67, 92)
+        Me.TableTournee.Name = "TableTournee"
+        Me.TableTournee.Size = New System.Drawing.Size(844, 376)
+        Me.TableTournee.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(291, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(454, 42)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "LISTE DES TOURNEES"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(429, 490)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(163, 39)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "RETOUR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(942, 192)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(92, 41)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "AJOUTER"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(942, 279)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(92, 38)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "SUPPRIMER"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'cmbBases
+        '
+        Me.cmbBases.FormattingEnabled = True
+        Me.cmbBases.Location = New System.Drawing.Point(99, 56)
+        Me.cmbBases.Name = "cmbBases"
+        Me.cmbBases.Size = New System.Drawing.Size(123, 30)
+        Me.cmbBases.TabIndex = 5
+        '
+        'cmbTable
+        '
+        Me.cmbTable.FormattingEnabled = True
+        Me.cmbTable.Location = New System.Drawing.Point(398, 56)
+        Me.cmbTable.Name = "cmbTable"
+        Me.cmbTable.Size = New System.Drawing.Size(147, 30)
+        Me.cmbTable.TabIndex = 6
+        '
+        'AC11
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Green
+        Me.ClientSize = New System.Drawing.Size(1062, 541)
+        Me.Controls.Add(Me.cmbTable)
+        Me.Controls.Add(Me.cmbBases)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TableTournee)
+        Me.Name = "AC11"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Lime
+        CType(Me.TableTournee, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TableTournee As System.Windows.Forms.DataGridView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents cmbBases As System.Windows.Forms.ListBox
+    Friend WithEvents cmbTable As System.Windows.Forms.ListBox
+
+End Class
