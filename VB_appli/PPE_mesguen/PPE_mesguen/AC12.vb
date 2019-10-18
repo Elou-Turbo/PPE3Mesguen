e3ed182c97c093a5bae42b32f324e7a17c3bbd15
OonnaC <oonna.chap@gmail.com>
2019-10-18 09:51:54 +0200

    Create AC12.vb

---
+++ VB_appli/PPE_mesguen/PPE_mesguen/AC12.vb
@@ -0,0 +1,30 @@
+﻿Public Class AC12
+
+    Dim myConnection As New Odbc.OdbcConnection
+    Dim myCommand As New Odbc.OdbcCommand
+    Dim myReader As Odbc.OdbcDataReader
+    Dim myAdapter As Odbc.OdbcDataAdapter
+    Dim myBuilder As Odbc.OdbcCommandBuilder
+    Dim connString As String
+    Dim donnee As DataTable
+
+    Private Sub AC12_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        
+        connString = "Dsn=BD_Oracle;uid=u_mesguen;Pwd=estran;"
+
+        myConnection.ConnectionString = connString
+
+        Try
+            myConnection.Open()
+            MessageBox.Show("Connexion Oracle Réussie")
+        Catch ex As Odbc.OdbcException
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub ChoixDateTournee_ValueChanged(sender As System.Object, e As System.EventArgs)
+
+
+    End Sub
+End Class
