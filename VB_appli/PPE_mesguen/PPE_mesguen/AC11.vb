3e0db1ce5e79effbcfd6f7ae9ab418729b699292
OonnaC <oonna.chap@gmail.com>
2019-10-18 09:09:49 +0200

    Update AC11.vb

--- VB_appli/PPE_mesguen/PPE_mesguen/AC11.vb
+++ VB_appli/PPE_mesguen/PPE_mesguen/AC11.vb
@@ -1,66 +1,80 @@
 ﻿Public Class AC11
-    Dim connexionSQL As MySqlConnection
+    Dim connexionSQL As New Odbc.OdbcConnection
     Dim donnee As DataTable
     Dim da As MySqlDataAdapter
     Dim cb As MySqlCommandBuilder
 
     Private Sub AC11_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        Dim Chaine As String
-        Chaine = "server=127.0.0.1 ; port=3307 ; userid=root ; password=; database=mlr2; pooling=false"
-        connexionSQL = New MySqlConnection(Chaine)
-        connexionSQL.open()
-        MessageBox.Show("Connexion Reussi")
+        'TODO: cette ligne de code charge les données dans la table 'DataSet1.DataTable2'. Vous pouvez la déplacer ou la supprimer selon vos besoins.
+        'Me.DataTable2TableAdapter.Fill(Me.DataSet1.DataTable2)
+        'Dim Chaine As String
+        'Chaine = "server=127.0.0.1 ; port=3307 ; userid=root ; password=; database=mlr2; pooling=false"
+        'connexionSQL = New MySqlConnection(Chaine)
+        'connexionSQL.open()
+        'MessageBox.Show("Connexion Reussi")
 
-        Dim reader As MySqlDataReader
-        Dim cmd As New MySqlCommand("SHOW DATABASES", connexionSQL)
-        reader = cmd.ExecuteReader()
-        cmbBases.Items.Clear()
-        While (reader.Read())
-            cmbBases.Items.Add(reader.GetString(0))
-        End While
-        reader.Close()
+        'Dim reader As MySqlDataReader
+        'Dim cmd As New MySqlCommand("SHOW DATABASES", connexionSQL)
+        'reader = cmd.ExecuteReader()
+        'cmbBases.Items.Clear()
+        'While (reader.Read())
+        '    cmbBases.Items.Add(reader.GetString(0))
+        'End While
+        'reader.Close()
     End Sub
 
    
 
 
 
-    Private Sub cmbBases_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cmbBases.SelectedIndexChanged
-        Dim reader2 As MySqlDataReader
-        connexionSQL.ChangeDatabase(cmbBases.SelectedItem.ToString())
-        Dim cmd2 As New MySqlCommand("SHOW TABLES", connexionSQL)
-        reader2 = cmd2.ExecuteReader()
-        cmbTable.Items.Clear()
-        While (reader2.Read())
-            cmbTable.Items.Add(reader2.GetString(0))
-        End While
-        reader2.Close()
+    Private Sub cmbBases_SelectedIndexChanged(sender As System.Object, e As System.EventArgs)
+        'Dim reader2 As MySqlDataReader
+        'connexionSQL.ChangeDatabase(cmbBases.SelectedItem.ToString())
+        'Dim cmd2 As New MySqlCommand("SHOW TABLES", connexionSQL)
+        'reader2 = cmd2.ExecuteReader()
+        'cmbTable.Items.Clear()
+        'While (reader2.Read())
+        '    cmbTable.Items.Add(reader2.GetString(0))
+        'End While
+        'reader2.Close()
 
     End Sub
 
-    Private Sub cmbTable_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles cmbTable.SelectedIndexChanged
+    Private Sub cmbTable_SelectedIndexChanged(sender As System.Object, e As System.EventArgs)
 
 
-        MsgBox(cmbTable.SelectedItem.ToString())
-        donnee = New DataTable
-        da = New MySqlDataAdapter("Select * from " + cmbTable.SelectedItem.ToString(), connexionSQL)
-        cb = New MySqlCommandBuilder(da)
-        da.Fill(donnee)
-        TableTournee.DataSource = donnee
+        'MsgBox(cmbTable.SelectedItem.ToString())
+        'donnee = New DataTable
+        'da = New MySqlDataAdapter("Select * from " + cmbTable.SelectedItem.ToString(), connexionSQL)
+        'cb = New MySqlCommandBuilder(da)
+        'da.Fill(donnee)
+        'TableTournee.DataSource = donnee
     End Sub
 
     Private Sub TableTournee_CellContentClick(sender As System.Object, e As System.Windows.Forms.DataGridViewCellEventArgs) Handles TableTournee.CellContentClick
-
-        MsgBox(cmbTable.SelectedItem.ToString())
-        donnee = New DataTable
-        da = New MySqlDataAdapter("Select * from ", connexionSQL)
-        cb = New MySqlCommandBuilder(da)
-        da.Fill(donnee)
-        TableTournee.DataSource = donnee
+        'da = New MySqlDataAdapter("Select  from " + cmbTable.SelectedItem.ToString(), connexionSQL)
+        'MsgBox(cmbTable.SelectedItem.ToString())
+        'donnee = New DataTable
+        'da = New MySqlDataAdapter("Select * from ", connexionSQL)
+        'cb = New MySqlCommandBuilder(da)
+        'da.Fill(donnee)
+        'TableTournee.DataSource = donnee
     End Sub
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
         Form_Connexion.Show()
         Me.Close()
     End Sub
+
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        'AC12.show()
+    End Sub
+
+    'Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+    '    Dim trnum As Integer
+    '    trnum = TableTournee.CurrentRow.Cells.Item(0).Value
+    '    myCommand = New Odbc.OdbcCommand("DELETE FROM ETAPE WHERE TRNNUM = " & trnum & ";", connexionSQL)
+    '    myCommand.ExecuteNonQuery()
+    'End Sub
 End Class
