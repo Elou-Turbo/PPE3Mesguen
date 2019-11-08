490201b3c0bb1674228594587b41691f16ed4e37
OonnaC <oonna.chap@gmail.com>
2019-11-08 11:29:20 +0100

    Update AC12.vb

--- VB_appli/PPE_mesguen/PPE_mesguen/AC12.vb
+++ VB_appli/PPE_mesguen/PPE_mesguen/AC12.vb
@@ -35,7 +35,7 @@
 
     Public Sub ListNomChauf_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListNomChauf.SelectedIndexChanged
 
-        'Dim dt As New DataTable("CHAUFFEUR")
+        'myReader.Close()
 
 
         ''TEST3
@@ -77,15 +77,15 @@
         '    ListNomChauf.Items.Add(myReader.GetString(1))
         'End While
 
-        Dim query As String = "SELECT CHFID,CHFNOM FROM CHAUFFEUR"
-        donnee = New DataTable
-        myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
-        myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
-        myAdapter.Fill(donnee)
+        'Dim query As String = "SELECT CHFID,CHFNOM FROM CHAUFFEUR"
+        'donnee = New DataTable
+        'myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
+        'myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
+        'myAdapter.Fill(donnee)
 
-        ListNomChauf.ValueMember = "CHFID"
-        ListNomChauf.DisplayMember = "CHFNOM"
-        ListNomChauf.DataSource = "CHAUFFEUR"
+        'ListNomChauf.ValueMember = "CHFID"
+        'ListNomChauf.DisplayMember = "CHFNOM"
+        'ListNomChauf.DataSource = "CHAUFFEUR"
 
     End Sub
 
@@ -112,6 +112,21 @@
     End Sub
 
     Private Sub ButtonAjout_Click(sender As System.Object, e As System.EventArgs) Handles ButtonAjout.Click
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+
+        Dim query As String = "SELECT CHFID,CHFNOM FROM CHAUFFEUR"
+        donnee = New DataTable
+        myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
+        myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
+        myAdapter.Fill(donnee)
+
+
+        ListBox1.DisplayMember = "CHFNOM"
+        ListBox1.ValueMember = "CHFID"
+        ListBox1.DataSource = donnee
 
     End Sub
 End Class
