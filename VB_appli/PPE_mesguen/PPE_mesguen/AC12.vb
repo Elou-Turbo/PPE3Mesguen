fad87b3c180e4f32166e450a834414ac6c6ad57e
OonnaC <oonna.chap@gmail.com>
2019-11-15 09:50:30 +0100

    Update AC12.vb

--- VB_appli/PPE_mesguen/PPE_mesguen/AC12.vb
+++ VB_appli/PPE_mesguen/PPE_mesguen/AC12.vb
@@ -9,14 +9,16 @@
     Dim donnee As DataTable
     Dim ds As DataSet
 
-
     Private Sub AC12_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        'TODO: cette ligne de code charge les données dans la table 'DataSet1.CHAUFFEUR'. Vous pouvez la déplacer ou la supprimer selon vos besoins.
+        'TODO: cette ligne de code charge les données dans la table 'DataSet1.VEHICULE'. Vous pouvez la déplacer ou la supprimer selon vos besoins.
+        Me.VEHICULETableAdapter.Fill(Me.DataSet1.VEHICULE)
 
+        'Connexion à la base MESGUEN
         connString = "Dsn=CNXORA_Mesguen;uid=u_mesguen;Pwd=estran;"
 
         myConnection.ConnectionString = connString
 
+        'Test de connexion à la base
         Try
             myConnection.Open()
             MessageBox.Show("Connexion Oracle Réussie")
@@ -24,17 +26,30 @@
             MessageBox.Show(ex.Message)
         End Try
 
-        Dim query As String = "SELECT CHFID,CHFNOM FROM CHAUFFEUR"
-        donnee = New DataTable
-        myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
-        myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
+        'Affichage de la liste des noms des chauffeurs
+        'Aide: http://net-informations.com/q/faq/combovalue.html
+        'Dim query As String = "SELECT CHFID,CHFNOM FROM CHAUFFEUR;"
+        'donnee = New DataTable
+        'myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
+        'myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
 
-        ListBox1.DisplayMember = "CHFNOM"
-        ListBox1.ValueMember = "CHFID"
-        ListBox1.DataSource = donnee
+        'ListNomChauf.DataSource = donnee
+        'ListNomChauf.DisplayMember = "CHFNOM"
+        'ListNomChauf.ValueMember = "CHFID"
+
+
+        'Affichage de la liste des chauffeurs
+        'Dim query As String = "SELECT VEHIMMAT FROM VEHICULE"
+        'donnee = New DataTable
+        'myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
+        'myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
+
+        'ListImmatVehi.DataSource = donnee
+        'ListImmatVehi.DisplayMember = "VEHIMMAT"
+        'ListImmatVehi.ValueMember = "VEHIMMAT"
+       
 
     End Sub
-
 
     Private Sub DateTimePicker1_ValueChanged(sender As System.Object, e As System.EventArgs) Handles DateTimePicker1.ValueChanged
         'Format JJ/MM/AAAA
@@ -43,63 +58,11 @@
 
     End Sub
 
-    Public Sub ListNomChauf_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListNomChauf.SelectedIndexChanged
-
-        'myReader.Close()
-
-
-        ''TEST3
-        'Dim result As String = "SELECT chaufid FROM CHAUFFEUR"
-        'Dim cmd As New OdbcCommand(result, myConnection)
-        'Dim da As OdbcDataAdapter = New OdbcDataAdapter(cmd)
-        'Dim dt As New DataTable("CHAUFFEUR")
-        'da.Fill(dt)
-        'If dt.Rows.Count > 0 Then
-        '    ListNomChauf.DataSource = dt
-        '    ListNomChauf.ValueMember = "chfid"
-        '    ListNomChauf.DisplayMember = "chfnom"
-        'End If
-
-        'ListNomChauf = ListNomChauf.SelectedValue
-
-        ''TEST2
-        'ListNomChauf.ValueMember = "chfid"
-        'ListNomChauf.DisplayMember = "chfnom"
-        'ListNomChauf.DataSource = "CHAUFFEUR"
-
-        ''TEST0
-        'Dim table_name As String = ListNomChauf.SelectedItem.ToString()
-        'Dim query As String = "SELECT chfNom FROM CHAUFFEUR INNER JOIN TOURNEE ON CHAUFFEUR.chfId = TOURNEE.chaufId;"
-        'donnee = New DataTable
-        'myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
-        'myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
-        'myAdapter.Fill(donnee)
-
-        'SocGrid.DataSource = donnee
-
-        'TEST1
-        'Dim query As String = "SELECT chfNom FROM CHAUFFEUR;"
-        'myCommand.Connection = myConnection
-        'myCommand.CommandText = query
-        'myReader = myCommand.ExecuteReader
-
-        'While (myReader.Read())
-        '    ListNomChauf.Items.Add(myReader.GetString(1))
-        'End While
-
-        'Dim query As String = "SELECT CHFID,CHFNOM FROM CHAUFFEUR"
-        'donnee = New DataTable
-        'myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
-        'myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
-        'myAdapter.Fill(donnee)
-
-        'ListNomChauf.ValueMember = "CHFID"
-        'ListNomChauf.DisplayMember = "CHFNOM"
-        'ListNomChauf.DataSource = "CHAUFFEUR"
+    Private Sub ListNomChauf_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListNomChauf.SelectedIndexChanged
 
     End Sub
 
-    Private Sub ListImmatVehi_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListImmatVehi.SelectedIndexChanged
+    Private Sub ListImmat_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListImmat.SelectedIndexChanged
 
     End Sub
 
@@ -122,21 +85,6 @@
     End Sub
 
     Private Sub ButtonAjout_Click(sender As System.Object, e As System.EventArgs) Handles ButtonAjout.Click
-
-    End Sub
-
-    Private Sub ListBox1_SelectedIndexChanged(sender As System.Object, e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
-
-        'Dim query As String = "SELECT CHFID,CHFNOM FROM CHAUFFEUR"
-        'donnee = New DataTable
-        'myAdapter = New Odbc.OdbcDataAdapter(query, myConnection)
-        'myBuilder = New Odbc.OdbcCommandBuilder(myAdapter)
-        'myAdapter.Fill(donnee)
-
-
-        'ListBox1.DisplayMember = "CHFNOM"
-        'ListBox1.ValueMember = "CHFID"
-        'ListBox1.DataSource = donnee
 
     End Sub
 
