9d06aa79932ae583f470bb0c5028c25b9f0041ec
OonnaC <oonna.chap@gmail.com>
2019-11-22 09:37:18 +0100

    Update DataSet1.vb

--- VB_appli/PPE_mesguen/PPE_mesguen/DataSet1.vb
+++ VB_appli/PPE_mesguen/PPE_mesguen/DataSet1.vb
@@ -1,4 +1,15 @@
 ﻿
 
 Partial Public Class DataSet1
+    Partial Class VEHICULEDataTable
+
+        Private Sub VEHICULEDataTable_ColumnChanging(sender As System.Object, e As System.Data.DataColumnChangeEventArgs) Handles Me.ColumnChanging
+            If (e.Column.ColumnName = Me.VEHIMMATColumn.ColumnName) Then
+                'Ajoutez le code utilisateur ici
+            End If
+
+        End Sub
+
+    End Class
+
 End Class
