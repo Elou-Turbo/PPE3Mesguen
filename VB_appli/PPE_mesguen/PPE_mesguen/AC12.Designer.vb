c84ca4c1765c348a9a88a2955c93e1baff76ce0f
OonnaC <35567072+OonnaC@users.noreply.github.com>
2019-11-28 20:34:52 +0100

    Update AC12.Designer.vb

--- VB_appli/PPE_mesguen/PPE_mesguen/AC12.Designer.vb
+++ VB_appli/PPE_mesguen/PPE_mesguen/AC12.Designer.vb
@@ -184,6 +184,7 @@ Partial Class AC12
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(594, 438)
         Me.Controls.Add(Me.ListLieuEtape)
         Me.Controls.Add(Me.ListImmat)
